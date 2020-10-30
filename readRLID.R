c8a11faa4f523105c4cf9eaac63835d75db30279
CHEN Dongmei <DCHEN@lcog.org>
2020-10-30 14:46:40 -0700

    corrected unique taxlot sales

--- readRLID.R
+++ readRLID.R
@@ -30,7 +30,7 @@ print(paste0('There are ', dim(valid.sales)[1], ' records of valid RLID sales', 
 # There are 254712 records of valid RLID sales, 
 # and 92.26% of the records are under a single account.
 
-unique.taxlot.sales <- single.acct.sales[unique(single.acct.sales$maplot),]
+unique.taxlot.sales <- single.acct.sales[!duplicated(single.acct.sales$maplot),]
 print(paste0('There are ', dim(single.acct.sales)[1], ' records of single account sales', ', but only ',
              round(dim(unique.taxlot.sales)[1]/dim(single.acct.sales)[1]*100, 2),
              '% of the records are single sales by taxlot.'))
@@ -41,6 +41,13 @@ comp.sales <- ReadFromSQL(database = 'RLID_WebApp_V3',
                           table = 'WEBAPP_COMP_SALES_COMP')
 
 dim(comp.sales)
-unique.taxlot.comp.sales <- comp.sales[unique(comp.sales$maplot),]
+unique.taxlot.comp.sales <- comp.sales[!duplicated(comp.sales$maplot),]
 
 # merge by maplot
+
+data <- merge(unique.taxlot.sales, unique.taxlot.comp.sales, 
+              by='maplot')
+data_new <- data[, colSums(is.na(data)) < nrow(data)]
+outfolder <- 'C:/Users/clid1852/OneDrive - lanecouncilofgovernments/RLID/'
+write.csv(data_new, paste0(outfolder, 'RLID_sales.csv'), 
+          row.names = FALSE)
