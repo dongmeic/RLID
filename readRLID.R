cd559c73763a4399adef8c63edd4c49791bf2ea7
CHEN Dongmei <DCHEN@lcog.org>
2020-10-31 01:02:29 -0700

    revised RLID sales dashboard

--- readRLID.R
+++ readRLID.R
@@ -37,17 +37,40 @@ print(paste0('There are ', dim(single.acct.sales)[1], ' records of single accoun
 # There are 235001 records of single account sales, 
 # but only 31.12% of the records are single sales by taxlot.
 
+# update with Nick's filters
+sales.subset <- subset(sales, arms_length == 'Y' & 
+                       recent_sale_ind == 'Y' & 
+                       group_sale_desc == 'S' &
+                       analysis_code %in% c('V', 'W'))
+
+sales.subset <- sales.subset[grep("SINGLE FAMILY", 
+                                  sales.subset$stat_class_desc, 
+                                  perl = T),]
+
+
 comp.sales <- ReadFromSQL(database = 'RLID_WebApp_V3', 
                           table = 'WEBAPP_COMP_SALES_COMP')
 
+drops <- names(comp.sales)[names(comp.sales) %in% names(sales.subset)][-1]
+
+comp.sales.subset <- subset(comp.sales, arms_length == 'Y' & 
+                       stat_class %in% sales.subset$stat_class)
+
+
 dim(comp.sales)
-unique.taxlot.comp.sales <- comp.sales[!duplicated(comp.sales$maplot),]
+unique.taxlot.comp.sales <- comp.sales.subset[!duplicated(comp.sales.subset$maplot),]
+unique.taxlot.sales <- unique.taxlot.comp.sales[,!(colnames(unique.taxlot.comp.sales) %in% drops)]
 
 # merge by maplot
 
-data <- merge(unique.taxlot.sales, unique.taxlot.comp.sales, 
+data <- merge(sales.subset, unique.taxlot.sales, 
               by='maplot')
 data_new <- data[, colSums(is.na(data)) < nrow(data)]
+data_new$sales_s <- round((data_new$sale_price - mean(data_new$sale_price))/sd(data_new$sale_price),0)
 outfolder <- 'C:/Users/clid1852/OneDrive - lanecouncilofgovernments/RLID/'
-write.csv(data_new, paste0(outfolder, 'RLID_sales.csv'), 
+data_test <- data_new[data_new$sale_price !=0 & data_new$sales_s <= 9,]
+data_test$sales_s <- round((data_test$sale_price - mean(data_test$sale_price))/sd(data_test$sale_price),0)
+
+write.csv(data_test, paste0(outfolder, 'RLID_sales.csv'), 
           row.names = FALSE)
+
